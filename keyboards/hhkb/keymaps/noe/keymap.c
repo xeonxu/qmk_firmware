642f4be78556bf7fbbd10c0ec606e25350b633e2
Noe Xu <noe.xu@clever-m.com>
2017-02-08 11:26:14 +0800

    Disable LSPO RSPC.

--- keyboards/hhkb/keymaps/noe/keymap.c
+++ keyboards/hhkb/keymaps/noe/keymap.c
@@ -24,7 +24,7 @@ enum keymap_layout {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* Layer 0: Default Layer
+    /* Layer 0: Safety Layer
      * ,-----------------------------------------------------------------------------------------.
      * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  \  |  `  |
      * |-----------------------------------------------------------------------------------------|
@@ -59,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [BASE] =
         KEYMAP(KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS, KC_GRV, \
-               KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC, \
-               KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_FN12, \
-	       KC_LSPO,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   MEH_T(KC_COMM),LCAG_T(KC_DOT), ALL_T(KC_SLSH),KC_RSPC,KC_FN0, \
+               KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   ALL_T(KC_LBRC),KC_RBRC,KC_BSPC, \
+               KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   MEH_T(KC_SCLN),LCAG_T(KC_QUOT),KC_FN12, \
+	       KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,KC_FN0, \
                                KC_FN4,KC_FN1,                KC_FN3,                 KC_RGUI,KC_FN5),
 
     /* Layer 2: Pseudo US Layout Layer */
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS, KC_GRV, \
                KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC, \
                KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_FN12, \
-	       KC_LSPO,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSPC,KC_FN0, \
-                               KC_FN4,KC_FN13,                KC_FN3,                 KC_RGUI,KC_FN5),
+	       KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,KC_FN0, \
+                               KC_FN4,KC_FN1,                KC_FN3,                 KC_RGUI,KC_FN5),
 
     /* Layer 4: Dvorak Layer
      * ,-----------------------------------------------------------------------------------------.
