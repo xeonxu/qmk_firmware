8ad6dc9f980ae3ba1d6c449673593f4d5fb8d76d
Xeon Xu <xeonxu@gmail.com>
2018-02-26 00:53:11 +0800

    Layer tap key not working

--- keyboards/satan/keymaps/minoer/keymap.c
+++ keyboards/satan/keymaps/minoer/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [BASE] = KEYMAP_HHKB(
-  KC_FN0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, \
+  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, \
   KC_FN2,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \
   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_FN6,  \
   KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(HHKB_EXTR), \
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *           `-----------------------------------------------------------------'
      */
     [MOUSE] = KEYMAP_HHKB(
-    KC_FN0, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL, \
+    KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL, \
     KC_TAB, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,KC_BTN1,KC_BTN3,KC_BTN2,KC_NO,KC_BTN4,KC_BTN5,     _______, \
     KC_CAPS,KC_NO,  KC_ACL0,KC_ACL1,KC_ACL2, KC_NO, KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,KC_NO,            KC_FN6, \
     KC_LSFT,     KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,KC_NO,       KC_RSFT, _______, \
@@ -151,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *           `-----------------------------------------------------------------'
      */
     [TENKEY] = KEYMAP_HHKB(
-	KC_FN0, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_PSLS,KC_PAST,KC_PMNS, KC_NO, KC_NO,  KC_NO, \
+	KC_ESC, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_PSLS,KC_PAST,KC_PMNS, KC_NO, KC_NO,  KC_NO, \
 	_______,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_KP_7,KC_KP_8,KC_KP_9, KC_PPLS,  KC_PPLS,KC_BSPC, \
 	_______,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_KP_4, KC_KP_5, KC_KP_6,  KC_PENT,  KC_PENT, \
 	_______,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_KP_1,KC_KP_2,KC_KP_3,_______,_______, \
@@ -162,17 +162,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * user defined action function
  */
-enum function_id {
-    SHIFT_ESC,
-};
+/* enum function_id { */
+/*     SHIFT_ESC, */
+/* }; */
 
 
 /*
  * Fn action definition
  */
 const uint16_t PROGMEM fn_actions[] = {
-    [0]  = ACTION_FUNCTION(SHIFT_ESC),
-    [1]  = ACTION_LAYER_TAP_KEY(MOUSE, KC_SPACE),
+    /* [0]  = ACTION_FUNCTION(SHIFT_ESC), */
+    [1]  = ACTION_LAYER_TAP_KEY(MOUSE, KC_SPC),
     [2]  = ACTION_LAYER_TAP_KEY(TENKEY, KC_TAB),
     [3]  = ACTION_DEFAULT_LAYER_SET(BASE),
     [4]  = ACTION_DEFAULT_LAYER_SET(MOUSE),
@@ -180,28 +180,28 @@ const uint16_t PROGMEM fn_actions[] = {
     [6]  = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_ENT),
 };
 
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-    static uint8_t shift_esc_shift_mask;
-    switch (id) {
-    case SHIFT_ESC:
-	shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
-	if (record->event.pressed) {
-	    if (shift_esc_shift_mask) {
-		add_key(KC_GRV);
-		send_keyboard_report();
-	    } else {
-		add_key(KC_ESC);
-		send_keyboard_report();
-	    }
-	} else {
-	    if (shift_esc_shift_mask) {
-		del_key(KC_GRV);
-		send_keyboard_report();
-	    } else {
-		del_key(KC_ESC);
-		send_keyboard_report();
-	    }
-	}
-	break;
-    }
-}
+/* void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) { */
+/*     static uint8_t shift_esc_shift_mask; */
+/*     switch (id) { */
+/*     case SHIFT_ESC: */
+/* 	shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK; */
+/* 	if (record->event.pressed) { */
+/* 	    if (shift_esc_shift_mask) { */
+/* 		add_key(KC_GRV); */
+/* 		send_keyboard_report(); */
+/* 	    } else { */
+/* 		add_key(KC_ESC); */
+/* 		send_keyboard_report(); */
+/* 	    } */
+/* 	} else { */
+/* 	    if (shift_esc_shift_mask) { */
+/* 		del_key(KC_GRV); */
+/* 		send_keyboard_report(); */
+/* 	    } else { */
+/* 		del_key(KC_ESC); */
+/* 		send_keyboard_report(); */
+/* 	    } */
+/* 	} */
+/* 	break; */
+/*     } */
+/* } */
