e04d7b096fd4364f7fb2f49c9b1a7a26265a29a2
Noe Xu <noe.xu@mrobot.cn>
2018-02-26 17:52:25 +0800

    Optimize tenkey layer.

--- keyboards/satan/keymaps/minoer/keymap.c
+++ keyboards/satan/keymaps/minoer/keymap.c
@@ -9,7 +9,7 @@
 
 enum keymap_layout {
     BASE = 0,
-    SAFE,
+    /* SAFE, */
     HHKB_EXTL,
     HHKB_EXTR,
     TENKEY,
@@ -18,25 +18,25 @@ enum keymap_layout {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap Safe: (Safe Layer) Safty Layer
-   * ,-----------------------------------------------------------.
-   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =| \ | ~ |
-   * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|bksp |
-   * |-----------------------------------------------------------|
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
-   * |-----------------------------------------------------------|
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift| fn |
-   * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |FN  |Ctrl |
-   * `-----------------------------------------------------------'
-   */
-[SAFE] = KEYMAP_HHKB(
-  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \
-  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
-  KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(HHKB_EXTR), \
-  KC_LCTL, KC_LALT, MO(HHKB_EXTL),          KC_SPC,                                               KC_RGUI, KC_RALT, KC_HYPR, KC_MEH),
+/*   /\* Keymap Safe: (Safe Layer) Safty Layer */
+/*    * ,-----------------------------------------------------------. */
+/*    * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =| \ | ~ | */
+/*    * |-----------------------------------------------------------| */
+/*    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|bksp | */
+/*    * |-----------------------------------------------------------| */
+/*    * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return | */
+/*    * |-----------------------------------------------------------| */
+/*    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift| fn | */
+/*    * |-----------------------------------------------------------| */
+/*    * |Ctrl|Gui |Alt |      Space            |Alt |Gui |FN  |Ctrl | */
+/*    * `-----------------------------------------------------------' */
+/*    *\/ */
+/* [SAFE] = KEYMAP_HHKB( */
+/*   KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, \ */
+/*   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \ */
+/*   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \ */
+/*   KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(HHKB_EXTR), \ */
+/*   KC_LCTL, KC_LALT, MO(HHKB_EXTL),          KC_SPC,                                               KC_RGUI, KC_RALT, KC_HYPR, KC_MEH), */
 
   /* Keymap Base: (Base Layer) Default Layer
    * ,-----------------------------------------------------------.
@@ -112,8 +112,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [LAYER_SET] = KEYMAP_HHKB(
-  DF(SAFE),  DF(BASE),  DF(MOUSE),  DF(TENKEY), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  \
+  _______, DF(BASE), DF(MOUSE), DF(TENKEY), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  \
   _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  \
   _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, \
   _______, _______, _______,                    _______,                                     _______, _______, _______, _______),
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [MOUSE] = KEYMAP_HHKB(
   KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL, \
   KC_TAB, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, KC_BTN4,KC_BTN5,     _______, \
-  KC_CAPS,XXXXXXX,  KC_ACL0,KC_ACL1,KC_ACL2, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, _______, XXXXXXX,  MT(MOD_RCTL,KC_ENT), \
+  KC_CAPS,XXXXXXX,  KC_ACL0,KC_ACL1,KC_ACL2, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,  MT(MOD_RCTL,KC_ENT), \
   KC_LSFT,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_WH_U, _______, \
   _______, _______, _______,                                _______,                            _______, KC_WH_L, KC_WH_D, KC_WH_R),
  /* Layer 6: Tenkey layer
@@ -151,11 +151,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *           `-----------------------------------------------------------------'
    */
   [TENKEY] = KEYMAP_HHKB(
-   KC_ESC, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PSLS,KC_PAST,KC_PMNS, XXXXXXX, XXXXXXX,  XXXXXXX, \
-   _______,XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_KP_7, KC_KP_8, KC_KP_9, KC_PPLS, KC_PPLS, KC_BSPC, \
-   _______,XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_KP_4, KC_KP_5, KC_KP_6, KC_PENT, KC_PENT, \
-   _______,XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, \
-   _______, _______, _______,                _______,                            KC_KP_0, KC_PDOT, _______, _______),
+   KC_ESC, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, KC_PSLS,KC_PAST,KC_PMNS, KC_PMNS, KC_PPLS, XXXXXXX, _______, \
+   _______,XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_KP_7, KC_KP_8, KC_KP_9, KC_PPLS, KC_PPLS, KC_PPLS, _______, \
+   _______,XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_KP_4, KC_KP_5, KC_KP_6, KC_PENT, KC_PENT, KC_PENT, \
+   _______,XXXXXXX, XXXXXXX,KC_CALCULATOR, XXXXXXX, XXXXXXX, XXXXXXX,  KC_KP_1, KC_KP_2, KC_KP_3, KC_PENT, KC_PENT, _______, \
+   _______, _______, _______,                                                   KC_KP_0, KC_PDOT, _______, _______, _______),
                                                               
 };
 
